082bcf7df5193c879c943112c3dc5eb0becf631e
kevinfernaldy <kevin.fernaldy@gmail.com>
2018-11-29 16:50:21 +0700

    Updated bank web service

--- bank web service/bank webservice.js
+++ bank web service/bank webservice.js
@@ -1,8 +1,9 @@
 var http = require("http");
 var url = require("url");
 var mysql = require("mysql");
+var fs = require("fs");
 
-var mysql_con = mysql.createConnection({host: "localhost", user: "root", password: "123", port: 3307});
+var mysql_con = mysql.createConnection({host: "localhost", user: "root", password: "kevin007", port: 3307});
 mysql_con.connect(function(e) {
 	if (e) throw e;
 
@@ -11,19 +12,24 @@ mysql_con.connect(function(e) {
 	});
 });
 
+let transfer_get;
+fs.readFile('./html/transfer_get.html','ascii',function (e, data){
+	transfer_get = data;
+});
+
 http.createServer(function(request,response){
 	var url_parts = url.parse(request.url,true);
-
 	switch (url_parts.pathname){
 		case ("/validate"):
-			if (request.method == "POST"){
+			if (request.method == "GET"){
+				console.log("validate GET invoked");
 				var validate_from_mysql = function(name, card_number){
 					var query = "SELECT * FROM nasabah WHERE name=\"" + name + "\" AND nomor_kartu=\"" + card_number +"\"";
 
 					mysql_con.query(query,function(e,result) {
 						if (e) throw e;
 
-						response.writeHead(200);
+						response.writeHead(200,{"Access-Control-Allow-Origin" : "http://localhost"});
 						if (result.length > 0){
 							console.log("Data available");
 							response.end("OK");
@@ -34,14 +40,45 @@ http.createServer(function(request,response){
 					});
 				};
 
+				var name = url_parts.query.name;
+				var card_number = url_parts.query.card_number;
+
+				validate_from_mysql(name,card_number);
+			} else {
+				response.writeHead(403);
+				response.end();
+			}
+			break;
+		case ("/transfer"):
+			if (request.method == "POST"){
 				request.on('data', function(data) {
 					var data_splitted = data.toString().split("&");
+					
+					var sender_card_num = (data_splitted[0].split("="))[1];
+					var receiver_card_num = (data_splitted[1].split("="))[1];
+					var amount = (data_splitted[2].split("="))[1];
 
-					var name = (data_splitted[0].split("=")[1]).split("+").join(" ");
-					var card_number = data_splitted[1].split("=")[1];
+					var query = "SELECT saldo FROM nasabah WHERE nomor_kartu=\"" + sender_card_num + "\"";
+					mysql_con.query(query,function(e, result) {
+						if (e) throw e;
 
-					validate_from_mysql(name,card_number);
+						response.writeHead(200);
+						if ((result[0].saldo - amount) >= 0){
+							console.log("Saldo cukup");
+							response.end("OK");
+
+							var query = "UPDATE nasabah SET saldo=" + (result[0].saldo - amount) + " WHERE nomor_kartu=\"" + sender_card_num + "\"";
+							
+							mysql_con.query(query,null);
+						} else {
+							console.log("Saldo kurang");
+							response.end();
+						}
+					});
 				});
+
+				response.writeHead(200);
+				response.end()
 			} else {
 				response.writeHead(403);
 				response.end();
@@ -51,6 +88,6 @@ http.createServer(function(request,response){
 			response.writeHead(404);
 			response.end();
 	}
-}).listen(8081)
+}).listen(8081);
 
 console.log("Listening on 127.0.0.1:8081");
---
+++ bank web service/transfer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bank ABC</title>
+		<link rel="stylesheet" type="text/css" href="./css/transfer.css">
+	</head>
+	<body>
+		<div class="main">
+			<?php
+				$name = $_GET["name"];
+				$amount = $_GET["amount"];
+			?>
+			<div>
+				<p>Welcome,</p>
+				<div id=name><?php echo $name;?></div>
+			</div>
+			<form action="http://localhost:8081/transfer" method="post">
+				<div class="table">
+					<table>
+						<tr>
+							<td id="idx_1">
+								Nomor kartu
+							</td>
+							<td>
+								<input type="text" name="no_kartu" id="card_num">
+							</td>
+							<td>
+								<img id="pic_res" src=""/>
+							</td>
+						</tr>
+						<tr>
+							<td></td>
+							<td id="error_res"></td>
+						</tr>
+						<tr>
+							<td id="idx_1">
+								Nomor kartu penerima
+							</td>
+							<td>
+								<input type="text" name="no_kartu_penerima" id="card_num2">
+							</td>
+						</tr>
+						<tr>
+							<td id="idx_1">
+								Jumlah
+							</td>
+							<td>
+								<input type="text" name="amount">
+							</td>
+						</tr>
+					</table>
+				</div>
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	</body>
+</html>
